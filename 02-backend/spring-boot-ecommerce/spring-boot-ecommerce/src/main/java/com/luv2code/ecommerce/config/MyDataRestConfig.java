b26b00b9406e9f20eb759920b85a3f21ed0d5bb0
davidjaguilar104 <davidjaguilar104@gmail.com>
2022-04-04 13:11:29 -0500

    fixed typo in REST api config

--- 02-backend/spring-boot-ecommerce/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/config/MyDataRestConfig.java
+++ 02-backend/spring-boot-ecommerce/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/config/MyDataRestConfig.java
@@ -20,14 +20,14 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
         // disable HTTP methods for Product: PUT, POST, and DELETE
         config.getExposureConfiguration()
                 .forDomainType(Product.class)
-                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)))
-                .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)));
+                .withItemExposure(((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions)))
+                .withCollectionExposure(((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions)));
 
         // disable HTTP methods for ProductCategory: PUT, POST, and DELETE
         config.getExposureConfiguration()
                 .forDomainType(ProductCategory.class)
-                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)))
-                .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)));
+                .withItemExposure(((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions)))
+                .withCollectionExposure(((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions)));
 
 
     }
