7bfdfd4f133c05863b91e8337850b391f2a22566
davidjaguilar104 <davidjaguilar104@gmail.com>
2022-04-15 11:13:21 -0500

    fixed object possibly null, removed a decoration

--- 03-frontend/angular-ecommerce/src/app/components/product-details/product-details.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/product-details/product-details.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from 'src/app/common/product';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.css']
+  styleUrls: ['./product-details.component.css'],
 })
 export class ProductDetailsComponent implements OnInit {
+  product!: Product;
 
-  constructor() { }
+  constructor(
+    private productService: ProductService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(() => {
+      this.handleProductDetails();
+    });
   }
 
+  handleProductDetails() {
+    // get the "id" param string and convert string to a number using the "+" symbol
+    let theProductId: any = this.route.snapshot.paramMap.get('id');
+    
+    const idNum = parseInt(theProductId);
+
+    this.productService.getProduct(idNum).subscribe((data) => {
+      this.product = data;
+    });
+  }
 }
--- 03-frontend/angular-ecommerce/src/app/services/product.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -15,10 +15,15 @@ export class ProductService {
 
   constructor(private httpClient: HttpClient) {}
 
+  getProduct(theProductId: number): Observable<Product> {
+    // need to build URL based on product id
+    const productUrl = `${this.baseUrl}/${theProductId}`;
+    return this.httpClient.get<Product>(productUrl);
+  }
+
   getProductList(theCategoryId: number): Observable<Product[]> {
     // need to build URL based on the category id
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
-
 
     // old method that we refactored to getProducts()
     // return this.httpClient
